44bd27999da4832ca279b269482f66995476d077
KingJamal_III <154438134+User-bf@users.noreply.github.com>
2025-11-16 14:35:27 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/MecanumDrive.java
@@ -94,7 +94,7 @@ public class MecanumDrive {
         // path controller gains
         public double axialGain = 15.0;
         public double lateralGain = 10.0;
-        public double headingGain = 10.0; // shared with turn
+        public double headingGain = 1.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
